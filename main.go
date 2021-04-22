ac4b33299af629de242e04dadc5aa7c99e129af9
Tarantini Pereira <taranttini@hotmail.com>
2021-04-22 02:20:04 -0300

    working with json

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 type fooHandler struct {
 	Message string
@@ -14,8 +18,56 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("bar called!!!"))
 }
 
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
+}
+
+func productToJsonHandler(w http.ResponseWriter, r *http.Request) {
+	product := &Product{
+		ProductID:      123,
+		Manufacturer:   "Big Box Company",
+		PricePerUnit:   "12.99",
+		Sku:            "4561qHJK",
+		Upc:            "414654444566",
+		QuantityOnHand: 28,
+		ProductName:    "Gizmo",
+	}
+	productJSON, err := json.Marshal(product)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(string(productJSON)))
+}
+
+func jsonToProductHandler(w http.ResponseWriter, r *http.Request) {
+	productJSON := `{
+		"productId": 123,
+		"manufacturer": "Big Box Company",
+		"sku": "4561qHJK",
+		"upc": "414654444566",
+		"pricePerUnit": "12.99",
+		"quantityOnHand": 28,
+		"productName": "Gizmo"
+	  }`
+
+	product := Product{}
+	err := json.Unmarshal([]byte(productJSON), &product)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte("json unmarshal product: " + product.ProductName))
+}
+
 func main() {
 	http.Handle("/foo", &fooHandler{Message: "foo called!!!"})
 	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/product-to-json", productToJsonHandler)
+	http.HandleFunc("/json-to-product", jsonToProductHandler)
 	http.ListenAndServe(":5000", nil)
 }
