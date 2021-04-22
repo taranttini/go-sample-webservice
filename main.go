ee924cdf55eb5f1912ab4f16b35903776be68bba
Tarantini Pereira <taranttini@hotmail.com>
2021-04-22 02:47:14 -0300

    workint with requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -27,6 +28,8 @@ type Product struct {
 	QuantityOnHand int    `json:"quantityOnHand"`
 	ProductName    string `json:"productName"`
 }
+
+var productList []Product
 
 func productToJsonHandler(w http.ResponseWriter, r *http.Request) {
 	product := &Product{
@@ -64,10 +67,93 @@ func jsonToProductHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("json unmarshal product: " + product.ProductName))
 }
 
+func init() {
+	jsonProductList := `[
+		{
+			"productId": 1,
+			"manufacturer": "Big Box Company",
+			"sku": "4561qHJK",
+			"upc": "414654444566",
+			"pricePerUnit": "12.99",
+			"quantityOnHand": 28,
+			"productName": "Gizmo"
+		},
+		{
+			"productId": 2,
+			"manufacturer": "Small Company",
+			"sku": "4561qABC",
+			"upc": "414654444533",
+			"pricePerUnit": "2.99",
+			"quantityOnHand": 5,
+			"productName": "Pencil"
+		},
+		{
+			"productId": 3,
+			"manufacturer": "Medium Company",
+			"sku": "4561qZYZ",
+			"upc": "414654444511",
+			"pricePerUnit": "5.98",
+			"quantityOnHand": 13,
+			"productName": "Photo"
+		}
+	]`
+	err := json.Unmarshal([]byte(jsonProductList), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNextID() int {
+	highestID := -1
+	for _, product := range productList {
+		if highestID < product.ProductID {
+			highestID = product.ProductID
+		}
+	}
+	return highestID + 1
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJson)
+	case http.MethodPost:
+		// add a new product to the list
+		var newProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if newProduct.ProductID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newProduct.ProductID = getNextID()
+		productList = append(productList, newProduct)
+		w.WriteHeader(http.StatusCreated)
+		return
+	default:
+		w.Write([]byte("method not defined!!!"))
+	}
+}
+
 func main() {
 	http.Handle("/foo", &fooHandler{Message: "foo called!!!"})
 	http.HandleFunc("/bar", barHandler)
 	http.HandleFunc("/product-to-json", productToJsonHandler)
 	http.HandleFunc("/json-to-product", jsonToProductHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
