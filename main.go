2495fae45f148015788e48ff1f997b9eaf8c1cf9
Tarantini Pereira <taranttini@hotmail.com>
2021-04-24 23:05:14 -0300

    uploading and downloading fles

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"inventoryservice/database"
 	"inventoryservice/product"
+	"inventoryservice/receipt"
+	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -12,6 +14,7 @@ const apiBasePath = "/api"
 
 func main() {
 	database.SetupDatabase()
+	receipt.SetupRoutes(apiBasePath)
 	product.SetupRoutes(apiBasePath)
-	http.ListenAndServe(":5000", nil)
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
